1607e8070b9db200b470996077e6922fecd0bd05
Canice James <canicejamesanu@gmail.com>
2017-11-15 23:58:33 -0400

    Linked script.js in portfolio page

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 
 window.onload = function() {
 
+  //Gets Expandable Paragraphs and Toggle elements
   var longParagraph = document.getElementsByClassName("content-long-paragraph");
   var readMoreButton = document.getElementsByClassName("read-more-button");
   
   for (var i = 0; i < longParagraph.length; i++){
     if (longParagraph[i].innerText.length > 120){
       if (readMoreButton[i] != null){
+
+        //Collapses all paragraphs by default
         collapseElement(longParagraph[i], readMoreButton[i]);
+
+        //Indexes Expandable Paragraphs and Toggle elements
         longParagraph[i].index = i;
         readMoreButton[i].index = i;
+
+        //Onclick listener for paragraph toggle
         readMoreButton[i].onclick = function(){
           toggleReadMore(longParagraph[this.index], readMoreButton[this.index]);
         }
@@ -23,7 +30,7 @@ function collapseElement(targetElement, toggleElement){
     targetElement.classList.remove("expanded");
   if (!targetElement.classList.contains("not-expanded"))
     targetElement.classList.add("not-expanded");
-  
+
   toggleElement.innerText = "Show More";
 }
 
