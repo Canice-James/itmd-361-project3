9e42e206e4b8b119b5bc62a59f15f75457add3a4
Canice James <canicejamesanu@gmail.com>
2017-11-15 23:41:49 -0400

    Implemented function to toggle collapse on paragraphs and set all paragraphs to collapse by default

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,38 @@
 window.onload = function() {
 
   var longParagraph = document.getElementsByClassName("content-long-paragraph");
+  var readMoreButton = document.getElementsByClassName("read-more-button");
   
-  
-  if (longParagraph[0].innerText.length > 120){
-    var charCount = longParagraph[0].innerText.length;
-    longParagraph[0].onclick = function(){
-      toggleReadMore(longParagraph[0]);
+  for (var i = 0; i < longParagraph.length; i++){
+    if (longParagraph[i].innerText.length > 120){
+      if (readMoreButton[i] != null){
+        collapseElement(longParagraph[i], readMoreButton[i]);
+        longParagraph[i].index = i;
+        readMoreButton[i].index = i;
+        readMoreButton[i].onclick = function(){
+          toggleReadMore(longParagraph[this.index], readMoreButton[this.index]);
+        }
+      }
     }
   }
 
 }
 
-function toggleReadMore(element){
+function collapseElement(targetElement, toggleElement){
+  if (targetElement.classList.contains("expanded"))
+    targetElement.classList.remove("expanded");
+  if (!targetElement.classList.contains("not-expanded"))
+    targetElement.classList.add("not-expanded");
+
+  toggleElement.innerText = "Show More";
+}
+
+function toggleReadMore(targetElement, toggleElement){
+  targetElement.classList.toggle("expanded");
+  targetElement.classList.toggle("not-expanded");
+
+  if (toggleElement.innerText == "Show More")
+    toggleElement.innerText = "Show Less";
+  else if (toggleElement.innerText == "Show Less")
+    toggleElement.innerText = "Show More";
 }
